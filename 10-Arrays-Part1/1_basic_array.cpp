230a591c8002a82ad37f12d4813321e0b4f1e850
hanifa-khanam <bc250220499bha@vu.edu.pk>
2026-02-15 23:15:28 +0500

    feat: Add new array examples and implementations for basic operations

--- 10-Arrays-Part1/1_basic_array.cpp
+++ 10-Arrays-Part1/1_basic_array.cpp
@@ -2,26 +2,12 @@
 using namespace std;
 
 int main() {
-    int marks[50];
-    cout << marks[0] << endl;
-    cout << marks[1] << endl;
-    cout << marks[48] << endl;
-    cout << marks[49] << endl;
-
-    cout << endl;
-    cout << "next" << endl;
-    cout << endl;
 
     int mark1[50] = {1, 2, 3};
     cout << mark1[0] << endl;
-    cout << mark1[1] << endl;
     cout << mark1[2] << endl;
-    cout << mark1[3] << endl;
     cout << mark1[49] << endl;
 
-    cout << endl;
-    cout << "next" << endl;
-    cout << endl;
 
     int mark2[] = {1, 2, 3};
     cout << mark2[0] << endl;

dfc2ed51e72bfc4b5cf79a039c5b3810ba4ec379
hanifa-khanam <bc250220499bha@vu.edu.pk>
2026-02-16 13:00:04 +0500

    style: Improve code formatting by adding whitespace for readability in binary search implementation

--- 10-Arrays-Part1/8_binay_search.cpp
+++ 10-Arrays-Part1/8_binay_search.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int binSearch(int arr[], int n, int key) {
+
     int start = 0, end = n - 1;
+    
     while (start <= end) {
         int mid = (start + end ) / 2;
         if(arr[mid] == key) {

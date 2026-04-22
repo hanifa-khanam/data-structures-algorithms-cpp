a0313f64384e807f0b37015f901eb098a82ab193
hanifa-khanam <bc250220499bha@vu.edu.pk>
2026-04-22 12:34:50 +0500

    feat: Enhance character array examples with additional string representations in C++

--- 14-Char-Arrays-and-Strings/1_character_array.cpp
+++ 14-Char-Arrays-and-Strings/1_character_array.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
     char arr[5] = {'a', 'b', 'c', 'd', '\0'};
     cout << arr[0] << endl;
     cout << arr[1] << endl;
-    cout << arr[2] << endl;
-    cout << arr[3] << endl;
-    cout << arr[4] << endl;
-
     cout << arr << endl;
+
+    cout << "Enter Sentence: ";
+    char sentence[50];
+    cin.getline(sentence, 50);
+
+    cout << "Sentence: " << sentence << endl;
+    cout << "Length: " << strlen(sentence) << endl;
+    
 
     return 0;
 }
--- 14-Char-Arrays-and-Strings/2_ways_of_writing.cpp
+++ 14-Char-Arrays-and-Strings/2_ways_of_writing.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main() {
     
+    char work[] = "code";
+    cout << work << endl;
+
+    char study[5] = "code";
+
+    char learn[] = {'c', 'o', 'd', 'e', '\0'};
+
+    char result[50] = {'c', 'o', 'd', 'e', '\0'};
 
     return 0;
-}
+}   

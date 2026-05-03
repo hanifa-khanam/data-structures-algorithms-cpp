0bbb8f373e4aa03accff6e37b132f6a31ffa05a7
hanifa-khanam <bc250220499bha@vu.edu.pk>
2026-05-03 11:37:27 +0500

    Add implementation of linked list to find the middle node

---
+++ 25-Linked-List/5_middle_node_of_linked_list.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+public:
+    Node* head;
+    Node* tail;
+
+    List() {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode->next = head;
+        head = newNode; 
+    }
+
+    Node* middleOfLL(Node* head) { 
+        if(head == NULL) {
+            cout << "empty Linked list\n";
+            return head;
+        }
+
+        Node* slow = head;
+        Node* fast = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+
+    }
+    
+    void printLL() {
+        Node* temp = head;
+
+        while(temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+};
+
+
+int main() {
+
+    List ll;
+    ll.push_front(6);
+    ll.push_front(5);
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    ll.printLL();
+
+    Node* mid = ll.middleOfLL(ll.head);
+
+    cout << "Middle node: " << mid->data << endl;
+
+
+    return 0;
+}

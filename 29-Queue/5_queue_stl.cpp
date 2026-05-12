e7bee73187751c2b376e3e5484b8cefdc46ed01f
hanifa-khanam <bc250220499bha@vu.edu.pk>
2026-05-12 19:06:18 +0500

    Add detailed output for queue operations including front, rear, size, and empty checks

--- 29-Queue/5_queue_stl.cpp
+++ 29-Queue/5_queue_stl.cpp
@@ -10,6 +10,29 @@ int main() {
     q.push(2);
     q.push(3);
 
+    cout << "Front Element: "
+         << q.front() << endl;
+
+    cout << "Rear Element: "
+         << q.back() << endl;
+
+    // Remove element
+    q.pop();
+
+    cout << "\nAfter pop:\n";
+
+    cout << "Front Element: " << q.front() << endl;
+
+    cout << "Queue Size: " << q.size() << endl;
+
+    // Check empty
+    if(q.empty()) {
+        cout << "Queue is empty\n";
+    }
+    else {
+        cout << "Queue is not empty\n";
+    }
+
     while(!q.empty()) {
         cout << q.front() << endl;
         q.pop();
